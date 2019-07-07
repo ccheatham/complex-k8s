254f4a875601c7c14df1e71686d6d24e7cd3eba9
Ched Cheatham <ched.cheatham@mindrally.com>
2019-07-07 15:02:23 -0500

    deploy.sh changes

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t ccheatham/complex-client:latest -t ccheatham/complex-client:$SHA -f ./client/Dockerfile ./client
 docker build -t ccheatham/complex-server:latest t ccheatham/complex-server:$SHA  -f  ./server/Dockerfile ./server
 docker build -t ccheatham/complex-worker:latest -t ccheatham/complex-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t ccheatham/complex-client:latest -t ccheatham/complex-client:$SHA -f ./client/Dockerfile ./client
 
-docker push ccheatham/complex-client:latest
 docker push ccheatham/complex-server:latest
+docker push ccheatham/complex-client:latest
 docker push ccheatham/complex-worker:latest
 
-docker push ccheatham/complex-client:$SHA
 docker push ccheatham/complex-server:$SHA
 docker push ccheatham/complex-worker:$SHA
+docker push ccheatham/complex-client:$SHA
 
-kubectl apply -f ./k8s
+kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=ccheatham/complex-server:$SHA
-kubectl set image deployments/client-deployment client=ccheatham/complex-client:$SHA
 kubectl set image deployments/worker-deployment worker=ccheatham/complex-worker:$SHA
+kubectl set image deployments/client-deployment client=ccheatham/complex-client:$SHA
