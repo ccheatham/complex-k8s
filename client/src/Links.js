0a4f65caa86b6ffae2a618a4cead91dd469158bb
Ched Cheatham <ched.cheatham@mindrally.com>
2019-09-06 01:27:32 -0500

    adds link to resume

--- client/src/Links.js
+++ client/src/Links.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
+      <a href="https://cheds-bucket.s3.us-east-1.amazonaws.com/chedcheatham-2019.pdf" target="_blank">Ched's CV/Resume</a>
+      <br />
       <Link to="/">Go back home</Link>
     </div>
   );
